2d4f6e20aa089480a5711d654f4a38c7662fff47
vadimkosolapov <vilgutis@icloud.com>
2022-09-07 12:30:44 +0500

    done

---
+++ src/task/Author.java
@@ -0,0 +1,19 @@
+package task;
+
+public class Author {
+    private String name;
+    private String lastName;
+
+    public Author(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/task/Book.java
@@ -0,0 +1,30 @@
+package task;
+
+public class Book {
+    private String name;
+    private Author author;
+    private int publicationYear;
+
+    public Book(String name, Author author, int publicationYear) {
+        this.name = name;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public int setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+        return publicationYear;
+    }
+}
